dcd986e4e37bb1a15e03d8448a35904a1b5380d3
Daniel Tobias Miguel <dan.tecnico@hotmail.com>
2026-08-04 19:08:07 -0300

    Sem precisar alterar o restante da aplicação.
    Porque futuramente poderemos criar:
    
    ambiente de desenvolvimento;
    ambiente de testes;
    ambiente de produção.

--- app.py
+++ app.py
@@ -2,22 +2,28 @@ from flask import Flask
 
 from config import Config
 from database import db
-
 from routes.chamados import chamados_bp
 
-# IMPORTANTE
 import models
 
-app = Flask(__name__)
 
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
 
-db.init_app(app)
+    app.config.from_object(Config)
 
-app.register_blueprint(chamados_bp)
+    db.init_app(app)
+
+    app.register_blueprint(chamados_bp)
+
+    return app
+
+
+app = create_app()
 
 with app.app_context():
     db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
