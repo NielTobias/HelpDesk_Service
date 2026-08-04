613b59bc814ef16a971909614718646ccc377ca0
Daniel Tobias Miguel <dan.tecnico@hotmail.com>
2026-08-04 18:50:25 -0300

    O app.py agora:
    cria a aplicação;
    carrega as configurações;
    conecta ao banco;
    registra os módulos.

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask
 from config import Config
 from database import db
 
+from routes.chamados import chamados_bp
+
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
 db.init_app(app)
 
-
-@app.route("/")
-def home():
-    return "HelpDesk Pro"
+app.register_blueprint(chamados_bp)
 
 
 if __name__ == "__main__":
